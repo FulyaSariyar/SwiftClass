c865ca29c3e7174bdeab6034d6aaa5af5b8de3f2
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-21 18:19:03 +0300

    Property, İnitializer

---
+++ MusicianClass/Musicians.swift
@@ -0,0 +1,21 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by Fulya Sarıyar on 21.06.2022.
+//
+
+import Foundation
+ //sınıf oluşturma
+class Musicians {
+    
+     //property
+    var name : String = ""
+    var age: Int = 0
+    var instrument : String = ""
+    
+    //initializer (Constructor)
+    init(){ //C# daki yapıcı metot
+        print("musician created")
+    }
+}
---
+++ MusicianClass/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Fulya Sarıyar on 21.06.2022.
+//
+
+import Foundation
+
+let james = Musicians() // james Musiciasn sınıfından oluşturulan bir obje
+//james.age = 50
+//james.name = "James Hetfield"
+//james.instrument = "Guitar"
+
+print(james.name)
+
+
